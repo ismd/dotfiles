f3c64f9aac2528cb641d4e621f8958ec6043323f
Vladimir Kosteley <github@ismd.dev>
2026-02-01 13:34:36 +0100

    feat(fish): update chezmoi abbreviations to avoid conflicts
    - Change abbreviation prefix from 'c' to 'cz' for clarity
    - Add 'czd' for chezmoi diff command
    - Prevent potential conflicts with other common 'c' prefixed commands

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -12,9 +12,10 @@ abbr -a ap arc pull
 abbr -a apush arc push
 abbr -a as arc status
 
-abbr -a c chezmoi
-abbr -a ca chezmoi apply
-abbr -a cs chezmoi status
+abbr -a cz chezmoi
+abbr -a cza chezmoi apply
+abbr -a czd chezmoi diff
+abbr -a czs chezmoi status
 
 abbr -a ga git add
 abbr -a gci git commit

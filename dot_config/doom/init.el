d404b07b418f76c1cf093cf535ad80ea97d42470
Vladimir Kosteley <github@ismd.dev>
2026-08-08 10:57:00 +0200

    feat(doom): enable qt module with lsp and tree-sitter support
    - Enable Doom Emacs qt module with +lsp and +tree-sitter flags
    - Add symlink for qmlls to dot_bin for easier access

--- dot_config/doom/init.el
+++ dot_config/doom/init.el
@@ -164,7 +164,7 @@
        ;;graphviz          ; diagrams for confusing yourself even more
        ;;purescript        ; javascript, but functional
        (python +lsp +tree-sitter) ; beautiful is better than ugly
-       ;;qt                ; the 'cutest' gui framework ever
+       (qt +lsp +tree-sitter) ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client

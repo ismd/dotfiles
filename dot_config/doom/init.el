0a041ecbc6927bdec6ed201ec061b813998a470b
Vladimir Kosteley <github@ismd.dev>
2026-08-08 16:05:49 +0200

    feat(doom): enable direnv and editorconfig modules in Doom Emacs config

--- dot_config/doom/init.el
+++ dot_config/doom/init.el
@@ -93,9 +93,9 @@
        ;;biblio            ; Writes a PhD for you (citation needed)
        ;;collab            ; buffers with friends
        debugger          ; stepping through code, to help you add bugs
-       ;;direnv
+       direnv
        docker
-       ;;editorconfig      ; let someone else argue about tabs vs spaces
+       editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        lookup              ; navigate your code and its documentation
